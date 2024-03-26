28c6d35e585bec2eb9c06f94763365f560a695e7
tunjan <trigo@inventati.org>
2024-03-26 12:51:41 +0100

    added dockerfile user permission and saving of output files

--- module2/example.py
+++ module2/example.py
@@ -3,6 +3,7 @@ import random
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
+import pandas as pd
 
 SEED = 42
 os.environ["PYTHONHASHSEED"] = str(SEED)
@@ -33,27 +34,20 @@ def preprocess_dataset(dataset):
     dataset = dataset.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
     dataset = dataset.cache()
     dataset = dataset.shuffle(ds_info.splits["train"].num_examples)
-    dataset = dataset.batch(128)
+    dataset = dataset.batch(200)
     dataset = dataset.prefetch(tf.data.AUTOTUNE)
     return dataset
 
 
+
 def build_model():
-    """Defines the CNN model for MNIST classification."""
-    model = tf.keras.models.Sequential(
-        [
-            tf.keras.layers.Conv2D(
-                32, (3, 3), activation="relu", input_shape=(28, 28, 1)
-            ),
-            tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
-            tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(64, activation="relu"),
-            tf.keras.layers.Dropout(0.2),
-            tf.keras.layers.Dense(10),
-        ]
-    )
+    model = tf.keras.models.Sequential([
+             tf.keras.layers.Flatten(input_shape=(28, 28)),
+             tf.keras.layers.Dense(200, activation='relu'),
+             tf.keras.layers.Dense(10)
+    ])
+
+
 
     model.compile(
         optimizer=tf.keras.optimizers.Adam(0.001),
@@ -66,7 +60,7 @@ def build_model():
 def load_and_preprocess_image(image_path):
     """Loads and preprocesses a single image for inference."""
     image = tf.keras.preprocessing.image.load_img(
-        image_path, grayscale=True, target_size=(28, 28)
+        image_path, target_size=(28, 28)
     )
     image_array = tf.keras.preprocessing.image.img_to_array(image)
     image_array = tf.expand_dims(image_array, axis=0)  # Add batch dimension
@@ -85,18 +79,20 @@ model.fit(
     validation_data=ds_test_preprocessed,
     callbacks=[tf.keras.callbacks.EarlyStopping(patience=2)],
 )  # Add early stopping
-model.save("mnist_model.keras")
-
-# Model Inference
-model = tf.keras.models.load_model("mnist_model.keras")
+model.save("output/mnist_model.keras")
 
 # Evaluation on the test set
 test_loss, test_accuracy = model.evaluate(ds_test_preprocessed)
 print("Test Loss:", test_loss)
 print("Test Accuracy:", test_accuracy)
 
-# Prediction on a sample image
-sample_image = load_and_preprocess_image("image.png")
-prediction = model.predict(sample_image)
-predicted_class = np.argmax(prediction[0])
-print("Predicted class:", predicted_class)
+predictions = model.predict(ds_test_preprocessed) 
+predicted_classes = np.argmax(predictions, axis=1)
+
+true_labels = []
+for x, y in ds_test_preprocessed:
+    true_labels.extend(y.numpy())  # Extend for each element in the batch
+
+# Create the DataFrame
+df = pd.DataFrame({'Predicted Class': predicted_classes, 'True Label': true_labels})
+df.to_csv('output/test_predictions.csv', index=False) 
